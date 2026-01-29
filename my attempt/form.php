e89d2a45016e57a8eff0c21730d1dd440b5b90bb
Vit Bojanovsky <vit.bojanovsky@outlook.cz>
2026-01-29 08:27:31 +0100

    tabulka

--- my attempt/form.php
+++ my attempt/form.php
@@ -4,9 +4,10 @@ $method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
 $raw = $_POST['oblibeny_dinosaurus'] ?? $_POST['oblibeny dinosaurus'] ?? null;
 $oblibeny_dinosaurus = trim((string)$raw);
 
-if ($oblibeny_dinosaurus === '') {
-    $oblibeny_dinosaurus = 'trex';
-}
+if ($oblibeny_dinosaurus === '')
+    {
+        $oblibeny_dinosaurus = 'ERROR_PRAZDNY_INPUT';
+    }
 
 ?>
 <!DOCTYPE html>
@@ -17,7 +18,11 @@ if ($oblibeny_dinosaurus === '') {
     <title>Form Response</title>
 </head>
 <body>
-    <h1><?php echo htmlspecialchars("tvuj oblibeny dinosaurus je " . $oblibeny_dinosaurus, ENT_QUOTES, 'UTF-8'); ?></h1>
-    
+    <?php for ($i = 0;$i<20;$i++) {
+    echo htmlspecialchars($i+1 . " tvuj oblibeny dinosaurus je " . $oblibeny_dinosaurus, ENT_QUOTES, 'UTF-8'); 
+    ?>
+    <br>
+<?php
+    } ?>
 </body>
 </html>
