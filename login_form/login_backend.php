1d0b602489e6604d9dac40396e9df067f7228bf9
Vit Bojanovsky <vit.bojanovsky@outlook.cz>
2026-02-09 14:21:49 +0100

    login form

---
+++ login_form/login_backend.php
@@ -0,0 +1,46 @@
+<?php 
+$auth = false;
+session_start();
+$heslo = $_POST['password'] ?? '';
+$jmeno = $_POST['username'] ?? '';
+
+$uzivatele = ['admin', 'vitecek','jirka'];
+$tajny_hesla = ['admin123', 'vitecek123', 'jirka123'];
+
+if(!isset($heslo) or !isset($jmeno)) {
+    echo("Neplatny input");
+}
+try {
+    if(count($uzivatele) == count($tajny_hesla)) {
+    } 
+    else {
+        throw new Exception();
+    }
+}
+catch (Exception $e) {
+    //pocet hesel a uzivatelu se neshoduje
+    echo("Interni chyba");
+}
+
+
+for($i = 0; $i<count($uzivatele); $i++) {
+    if($heslo == $tajny_hesla[$i] and $jmeno == $uzivatele[$i]) {
+        $auth = true;
+        break;
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prihlasovaci udaje</title>
+    <h1><?php if($auth) { echo "Prihlaseni uspesne"; } else { echo "Neplatny login"; } ?></h1>
+    <p><?php echo("zadane username = " . $jmeno); ?></p>
+    <p><?php echo("zadane heslo = " . $heslo); ?></p>
+</head>
+<body>
+    
+</body>
+</html>
